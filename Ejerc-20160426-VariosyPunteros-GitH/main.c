f36d4b15219299cace0fab09bb7ed87eecb72c1b
MDM IOS DEV <MDMIOSDEV@iMac-de-MDM.local>
2016-04-26 19:06:01 +0200

    Initial Commit

---
+++ Ejerc-20160426-VariosyPunteros-GitH/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Ejerc-20160426-VariosyPunteros-GitH
+//
+//  Created by MDM IOS DEV  on 26/04/16.
+//  Copyright (c) 2016 curso_DAM_AEG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
